d18ea420fb23234e388cab44112ad71ad851f411
mvrush <mvrush@gmail.com>
2021-11-15 17:53:03 -0700

    Working on W10

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,7 +1,18 @@
 exports.getPosts = (req, res, next) => {
 // this response is sent in json format. The json() function is built in and takes the JavaScript object {} and formats it as JSON with the correct headers etc.
     res.status(200).json({ // we have to be clear on our status codes so the client knows if there was an error or not. A 200 code means success.
-        posts: [{ title: 'First Post', content: 'This is the first post!' }]
+        posts: [
+            { 
+                _id: '1',
+                title: 'First Post',
+                content: 'This is the first post!',
+                imageUrl: 'images/mexican_austin.jpg',
+                creator: {
+                    name: 'Big Willy'
+                },
+                createdAt: new Date()
+            }
+        ]
     });
 };
 
@@ -11,6 +22,12 @@ exports.createPost = (req, res, next) => {
     // NOTE: we will create a post in our database at some point
     res.status(201).json({ // status 200 means success, status 201 means success AND we created a resource.
         message: 'Post created successfully!',
-        post: { id: new Date().toISOString(), title: title, content: content }
+        post: {
+            _id: new Date().toISOString(),
+            title: title,
+            content: content,
+            creator: { name: 'Big Willy'},
+            createdAt: new Date()
+        }
     });
 };
